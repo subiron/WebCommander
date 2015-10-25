98ee73568b888691460b8a27fa6c6a870c594f41
Tomasz Misiewicz <misiewicz.tomasz@gmail.com>
2015-10-25 14:27:41 +0100

    format go source aded intelij folder to gitignore

--- src/server.go
+++ src/server.go
@@ -5,57 +5,53 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-    "encoding/json"
+	"encoding/json"
 
 
-    //"container/list"
+//"container/list"
 )
 
 type CommanderFile struct {
-            Title  string `json:"title"`
-            Size  int64 `json:"size"`
-            IsFolder  bool `json:"isFolder"`
+	Title    string `json:"title"`
+	Size     int64 `json:"size"`
+	IsFolder bool `json:"isFolder"`
 
 }
 
-type CommanderDir struct{
-files []CommanderFile
+type CommanderDir struct {
+	files []CommanderFile
 }
 
 
 
 func main() {
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.HandleFunc("/", service)
+	http.Handle("/s/", http.StripPrefix("/s/", http.FileServer(http.Dir("."))))
 	log.Println("Listening...")
 	http.ListenAndServe(":3000", nil)
 }
 
 
 func service(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 
-    dirname:="static"
-    fileInfoList, error := ioutil.ReadDir(dirname)
-    if error != nil {
-        fmt.Fprintf(w, " :( Hi there, I love %s!", r.URL.Path[1:])
-    }
+	dirname := "static"
+	fileInfoList, error := ioutil.ReadDir(dirname)
+	if error != nil {
+		fmt.Fprintf(w, " :( Hi there, I love %s!", r.URL.Path[1:])
+	}
+
+	var cDir CommanderDir;
+	cDir.files = make([]CommanderFile, 0)
+	for _, dir := range fileInfoList {
+		//extract data to our struct
+		cDir.files = append(cDir.files, CommanderFile{dir.Name(), dir.Size(), dir.IsDir()})
+	}
 
 
-var cDir CommanderDir;
-cDir.files := make([]CommanderFile,0)
-    for _, dir := range fileInfoList {
-    //extract data to our struct
-        cf:=CommanderFile{dir.Name(),dir.Size(),dir.IsDir()}
-        cDir.files.PushBack(cf);
-    }
-	
-
-    j, err := json.Marshal(cDir)
-    if err != nil {
-        fmt.Fprintf(w, " :( Hi there, I love %s!", r.URL.Path[1:])
-    }else{
-        fmt.Fprintf(w,"%s\n", j)
-    }
+	j, err := json.Marshal(cDir)
+	if err != nil {
+		fmt.Fprintf(w, " :( Hi there, I love %s!", r.URL.Path[1:])
+	}else {
+		fmt.Fprintf(w, "%s\n", j)
+	}
 }
