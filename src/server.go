b7fe56e20f581a2b170160b851d68ee4b50ee7d3
Tomasz Misiewicz <misiewicz.tomasz@gmail.com>
2015-11-24 01:27:23 +0100

    added basic websocket server

--- src/server.go
+++ src/server.go
@@ -1,35 +1,69 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"encoding/json"
+	"os"
+	"strings	"
+	"github.com/gorilla/websocket"
 
-
-//"container/list"
+	//"container/list"
 )
 
 type CommanderFile struct {
 	Title    string `json:"title"`
-	Size     int64 `json:"size"`
-	IsFolder bool `json:"isFolder"`
-
+	Size     int64  `json:"size"`
+	IsFolder bool   `json:"isFolder"`
 }
 
 type CommanderDir struct {
 	files []CommanderFile
 }
 
-
-
 func main() {
+
 	http.Handle("/s/", http.StripPrefix("/s/", http.FileServer(http.Dir("."))))
+	http.HandleFunc("/ws/", wsHandler)
 	log.Println("Listening...")
+
 	http.ListenAndServe(":3000", nil)
 }
 
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("wshandler")
+	var upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer conn.Close()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for {
+		messageType, message, err := conn.ReadMessage()
+		if err != nil {
+			return
+		}
+		log.Print(message)
+
+		err = conn.WriteMessage(messageType, []byte(strings.Join(getdrives(), ",")))
+		if err != nil {
+			return
+		}
+	}
+}
 
 func service(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
@@ -40,18 +74,27 @@ func service(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, " :( Hi there, I love %s!", r.URL.Path[1:])
 	}
 
-	var cDir CommanderDir;
+	var cDir CommanderDir
 	cDir.files = make([]CommanderFile, 0)
 	for _, dir := range fileInfoList {
 		//extract data to our struct
 		cDir.files = append(cDir.files, CommanderFile{dir.Name(), dir.Size(), dir.IsDir()})
 	}
 
-
 	j, err := json.Marshal(cDir)
 	if err != nil {
 		fmt.Fprintf(w, " :( Hi there, I love %s!", r.URL.Path[1:])
-	}else {
+	} else {
 		fmt.Fprintf(w, "%s\n", j)
 	}
+}
+
+func getdrives() (r []string) {
+	for _, drive := range "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
+		_, err := os.Open(string(drive) + ":\\")
+		if err == nil {
+			r = append(r, string(drive))
+		}
+	}
+	return
 }
